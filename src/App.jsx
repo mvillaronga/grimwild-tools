2c386efd0f77521148aa858c0f7dd801bd3f49ee
michael villaronga <michael.villaronga@gmail.com>
2025-05-23 10:56:59 -0700

    project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Grimwild Tools React App</h1>
+      {/* Add your components here */}
+    </div>
+  );
+}
+
+export default App;
