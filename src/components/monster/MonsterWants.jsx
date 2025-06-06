ce506a7bdf6394a28e42db3d88a7796909636f4c
michael villaronga <michael.villaronga@gmail.com>
2025-06-06 11:24:00 -0700

    refactor: clean up wants and dislikes strings in MonsterWants component

--- src/components/monster/MonsterWants.jsx
+++ src/components/monster/MonsterWants.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default function MonsterWants({ wants, dislikes }) {
+  const cleanedWants = wants.replace(/^\s*Wants\s*/i, "").trim();
+  const cleanedDislikes = dislikes.replace(/^\s*Doesn't want\s*/i, "").trim();
+
   return (
     <p>
       <em className="bold">
         <strong>Wants</strong>
       </em>{" "}
-      <em>{wants}</em>
+      <em>{cleanedWants}</em>
       <br />
       <em className="bold">
         <strong>Doesn't want</strong>
       </em>{" "}
-      <em>{dislikes}</em>
+      <em>{cleanedDislikes}</em>
     </p>
   );
 }
