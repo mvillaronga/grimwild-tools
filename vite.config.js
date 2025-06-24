78780f9a413da2e739c32a5f22850d9f117897a0
michael villaronga <michael.villaronga@gmail.com>
2025-06-24 10:19:08 -0700

    fix deploy issues

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/grimwild-tools'
+  base: '/grimwild-tools',
+  build: {
+    emptyOutDir: false,
+    outDir: 'dist'
+  }
 });
